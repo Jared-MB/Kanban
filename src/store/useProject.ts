1b5b74e1f976036b3128ed6c1feb672b4b6f1e4c
Jared Muñoz <amunozbaez669@gmail.com>
2024-05-01 16:03:58 -0600

    Refactor useProject.ts to improve code readability and maintainability

--- src/store/useProject.ts
+++ src/store/useProject.ts
@@ -1,5 +1,5 @@
 import type { Project } from "@/types/project";
-import { Task } from "@/types/task";
+import type { Task } from "@/types/task";
 import { create } from "zustand";
 import { combine, persist } from "zustand/middleware";
 
@@ -12,7 +12,11 @@ export const useProject = create(
 			},
 			(set) => ({
 				setActiveProject: (project: Project) => set({ activeProject: project }),
-				addProject: (project: Project) => set((state) => ({ ...state, projects: [...state.projects, project] })),
+				addProject: (project: Project) =>
+					set((state) => ({
+						...state,
+						projects: [...state.projects, project],
+					})),
 				addTask: (task: Task, status: "pending" | "progress" | "completed") => {
 					set((state) => {
 						const project = state.projects.find(
@@ -20,53 +24,82 @@ export const useProject = create(
 						);
 						if (!project) return state;
 						project.tasks[status].push(task);
-						const activeProject = { ...state.activeProject, tasks: project.tasks } as Project;
+						const activeProject = {
+							...state.activeProject,
+							tasks: project.tasks,
+						} as Project;
 						return { activeProject, projects: state.projects };
 					});
 				},
-				removeTask: (taskId: string, status: 'pending' | 'progress' | 'completed') => {
+				removeTask: (
+					taskId: string,
+					status: "pending" | "progress" | "completed",
+				) => {
 					set((state) => {
 						const project = state.projects.find(
 							(project) => project._id === state.activeProject?._id,
 						);
 						if (!project) return state;
-						project.tasks[status] = project.tasks[status].filter((task) => task._id !== taskId);
-						const activeProject = { ...state.activeProject, tasks: project.tasks } as Project;
-						return { activeProject, projects: state.projects };
-					});
-				}, 
-				updateTask: (task: Task, status: "pending" | "progress" | "completed") => {
-					set((state) => {
-						const project = state.projects.find(
-							(project) => project._id === state.activeProject?._id,
+						project.tasks[status] = project.tasks[status].filter(
+							(task) => task._id !== taskId,
 						);
-						if (!project) return state;
-						project.tasks[status] = project.tasks[status].map((t) => (t._id === task._id ? task : t));
-						const activeProject = { ...state.activeProject, tasks: project.tasks } as Project;
+						const activeProject = {
+							...state.activeProject,
+							tasks: project.tasks,
+						} as Project;
 						return { activeProject, projects: state.projects };
 					});
 				},
-				updateTaskStatus: (taskId: string, subtaskId: string, status: boolean, statusType: "progress" | "completed") => {
+				updateTask: (
+					task: Task,
+					status: "pending" | "progress" | "completed",
+				) => {
 					set((state) => {
 						const project = state.projects.find(
 							(project) => project._id === state.activeProject?._id,
 						);
 						if (!project) return state;
-						project.tasks[statusType] = project.tasks[statusType].map((task) => {
-							if (task._id === taskId) {
-								task.subtasks = task.subtasks.map((subtask) => {
-									if (subtask._id === subtaskId) {
-										subtask.completed = status;
-									}
-									return subtask;
-								});
-							}
-							return task;
-						});
-						const activeProject = { ...state.activeProject, tasks: project.tasks } as Project;
+						project.tasks[status] = project.tasks[status].map((t) =>
+							t._id === task._id ? task : t,
+						);
+						const activeProject = {
+							...state.activeProject,
+							tasks: project.tasks,
+						} as Project;
 						return { activeProject, projects: state.projects };
 					});
-				}
+				},
+				updateTaskStatus: (
+					taskId: string,
+					subtaskId: string,
+					status: boolean,
+					statusType: "progress" | "completed",
+				) => {
+					set((state) => {
+						const project = state.projects.find(
+							(project) => project._id === state.activeProject?._id,
+						);
+						if (!project) return state;
+						project.tasks[statusType] = project.tasks[statusType].map(
+							(task) => {
+								if (task._id === taskId) {
+									task.subtasks = task.subtasks.map((subtask) => {
+										if (subtask._id === subtaskId) {
+											subtask.completed = status;
+										}
+										return subtask;
+									});
+								}
+								return task;
+							},
+						);
+						const activeProject = {
+							...state.activeProject,
+							tasks: project.tasks,
+						} as Project;
+						return { activeProject, projects: state.projects };
+					});
+				},
 			}),
 		),
 		{
@@ -76,17 +109,19 @@ export const useProject = create(
 );
 
 export const useTask = () => {
-	const { addTask, removeTask, updateTask, updateTaskStatus } = useProject(state => ({
-		addTask: state.addTask,
-		removeTask: state.removeTask,
-		updateTask: state.updateTask,
-		updateTaskStatus: state.updateTaskStatus,
-	}))
+	const { addTask, removeTask, updateTask, updateTaskStatus } = useProject(
+		(state) => ({
+			addTask: state.addTask,
+			removeTask: state.removeTask,
+			updateTask: state.updateTask,
+			updateTaskStatus: state.updateTaskStatus,
+		}),
+	);
 
 	return {
 		addTask,
 		removeTask,
 		updateTask,
 		updateTaskStatus,
-	}
-}
+	};
+};
