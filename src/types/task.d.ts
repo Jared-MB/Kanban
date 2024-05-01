4da4c143756c8e33d7d6694bd866230b7f33b1bd
Jared Muñoz <amunozbaez669@gmail.com>
2024-05-01 16:03:14 -0600

    Add optional description field to Task and Subtasks interfaces

--- src/types/task.d.ts
+++ src/types/task.d.ts
@@ -1,11 +1,13 @@
 export interface Task {
 	_id: string;
 	title: string;
+	description?: string;
 	subtasks: Subtasks[];
 }
 
 export interface Subtasks {
 	_id: string;
 	name: string;
+	description?: string;
 	completed: boolean;
 }
