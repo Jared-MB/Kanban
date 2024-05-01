6869492d4a74d2b9894f8d6ea6de0efb5d6b43db
Jared Muñoz <amunozbaez669@gmail.com>
2024-05-01 16:03:22 -0600

    Add optional description field to TaskValidator and SubtaskValidator

--- src/validators/task.validator.ts
+++ src/validators/task.validator.ts
@@ -2,9 +2,11 @@ import { z } from "zod";
 
 const TaskValidator = z.object({
 	title: z.string().min(3).max(100),
+	description: z.string().optional(),
 	subtasks: z.array(
 		z.object({
 			name: z.string().min(3).max(100),
+			description: z.string().optional(),
 			completed: z.boolean().optional(),
 		}),
 	),
